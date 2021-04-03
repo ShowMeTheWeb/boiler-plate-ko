477ccb493ccee736bf5241ac0d212f128e202921
ShowMeTheWeb <vaccine1220@naver.com>
2021-04-04 02:01:07 +0900

    test1
    test1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const config = require('./config/key');
 
 const { auth } = require('./middleware/auth');
@@ -24,6 +23,11 @@ mongoose.connect(config.mongoURI, {
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello skchoi'));
+
+app.get('/api/hello', (req, res) => {
+    res.send('안녕하세요');
+});
+
 
 app.post('/api/users/register', (req, res) => {
     // 회원 가입 할때 필요한 정보들을 Client에서 가져오고 DB에 넣어준다.    
@@ -105,5 +109,6 @@ app.get('/api/users/logout', auth, (req, res) => {
         });
 });
 
+const port = 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
